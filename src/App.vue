10e422c2a6aa552bbbc8edc69abc107197e03d7a
nahsooyeon <nahsooyeon@gmail.com>
2022-02-01 18:07:07 +0900

    🎉 추가: Vue.js 샘플 코드

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item
+          :label="item.label"
+          :done="item.done"
+          :id="item.id"
+        ></to-do-item>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
-
+// import uniqueId from "lodash.uniqueid";
+import uniqueId from "lodash.uniqueid";
+import { Options, Vue } from "vue-class-component";
+import ToDoItem from "./components/ToDoItem.vue";
+import ToDoForm from "./components/ToDoForm.vue";
+/* 로컬 컴포넌트 등록하는 법,이 컴포넌트를 등록한 상위 컴포넌트에서만 사용가능. */
 @Options({
-  components: {
-    HelloWorld,
+  name: "app",
+  components: { ToDoItem, ToDoForm },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId("todo-"), label: "Learn Vue", done: false },
+        {
+          id: uniqueId("todo-"),
+          label: "Create a Vue project with the CLI",
+          done: true,
+        },
+        { id: uniqueId("todo-"), label: "Have fun", done: true },
+        { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
+      ],
+    };
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      /* Form의 label 값을 ToDOItem list 배열에 추가 */
+      this.ToDoItems.push({
+        id: uniqueId("todo-"),
+        label: toDoLabel,
+        done: false,
+      });
+    },
   },
 })
+
+/* 필수 */
 export default class App extends Vue {}
 </script>
 
